6edd71803ffb03d4e1b4dd7f32e6f913ec040215
erik <eryx466@gmail.com>
2019-06-18 20:46:53 +0700

    update validasi category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -16,69 +16,95 @@ exports.categories = function(req,res){
 
 exports.category = function (req,res){
     connect.query(`SELECT * FROM category WHERE id=${req.params.id}`, function (error, rows, fields){
-        if (error) {
-            console.log(error);
+        if (rows=="") {
+            return res.send({
+                error:true,
+                message : "failed name"
+            })
         }else{
-            response.ok(rows,res);
-        }
+            if (error) {
+                console.log(error);
+            }else{
+                response.ok(rows,res);
+            }
+        }  
     });
 };
 
 exports.add = function(req,res){
     let name = req.body.name;
-    
-
-    connect.query(
-        'INSERT INTO category SET name=?',
-        [name],
-        function (error, rows, fields){
-            if (error) {
-                throw error
-            }else{
-                return res.send({
-                    error:false,
-                    data: rows,
-                    message: "category has been created",
-                });
+    if (!name) {
+        return res.send({
+            error:true,
+            message : "failed name"
+        })
+    }else{
+        connect.query(
+            'INSERT INTO category SET name=?',
+            [name],
+            function (error, rows, fields){
+                if (error) {
+                    throw error
+                }else{
+                    return res.send({
+                        error:false,
+                        data: rows,
+                        message: "category has been created",
+                    });
+                }
             }
-        }
-    );
+        );
+    }
+
 };
 
 exports.update = function(req,res){
     let id = req.params.id;
     let name = req.body.name;
-    connect.query(
-        `update category set name="${name}" where id=${id}`,
-        
-        function (error, rows, fields){
-            if (error) {
-                throw error
-            }else{
-                return res.send({
-                    error:false,
-                    data: rows,
-                    message: "category has been Updated",
-                });
+    if (name=="") {
+        return res.send({
+            error:true,
+            message : "failed name"
+        });
+    }else{
+        connect.query(
+            `update category set name="${name}" where id=${id}`,
+            
+            function (error, rows, fields){
+                if (error) {
+                    throw error
+                }else{
+                    return res.send({
+                        error:false,
+                        data: rows,
+                        message: "category has been Updated",
+                    });
+                }
             }
-        }
-    );
+        );
+    }
 };
-
+//ERORR
 exports.delete = function(req,res){
     let id = req.params.id;
     connect.query(
         `delete from category where id=${id}`,
-        
         function (error, rows, fields){
-            if (error) {
-                throw error
-            }else{
+            if (rows.affectedRows=="") {
                 return res.send({
-                    error:false,
-                    data: rows,
-                    message: "category has been Deleted",
+                    error:true,
+                    message : `failed delete id ${id}`
                 });
+            }else{
+                if (error) {
+                    throw error
+                }else{
+                    return res.send({
+                        error:false,
+                        data: rows,
+                        message: "category has been Deleted",
+                    });
+                }
             }
         }
     );
